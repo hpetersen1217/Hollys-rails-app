0848497ed52af7db16f1e1b9adc0ff0f22b25221
HollyPetersen <h.petersen@live.com>
2013-12-26 14:10:41 -0800

    Implemented testing suite and created tests for Vote

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Bloccit::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'Hollys-rails-app.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'HollyPetersen-bloccit.herokuapp.com'}
 end
